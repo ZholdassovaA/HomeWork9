289f9c6a53deab7ecc2ed941e3f2b924de0e4584
Aidana <zholdassova.aidana@mail.ru>
2022-02-26 00:40:12 +0600

    added attachments

--- src/test/java/lesson9/fillFormTest/RegistrationFormTest.java
+++ src/test/java/lesson9/fillFormTest/RegistrationFormTest.java
@@ -2,11 +2,14 @@ package lesson9.fillFormTest;
 
 import com.github.javafaker.Faker;
 import lesson9.jenkins.TestBase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
+
+@DisplayName("Заполнение формы регистрации")
 
 public class RegistrationFormTest extends TestBase {
 
