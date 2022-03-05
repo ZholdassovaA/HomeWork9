1edb454ea5a3d5206d7ccfa625a763324df8f8a9
Aidana <zholdassova.aidana@mail.ru>
2022-03-06 01:47:33 +0600

    added OwnerTest

--- src/test/java/lesson9/jenkins/TestBase.java
+++ src/test/java/lesson9/jenkins/TestBase.java
@@ -1,19 +1,25 @@
 package lesson9.jenkins;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static java.lang.String.format;
+
 public class TestBase {
     @BeforeAll
-    static void setup() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    static void beforeAll() {
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+        String login = credentials.login();
+        String password = credentials.password();
+        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub/");
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = format("https://%s:%s@%s", login, password, url);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
