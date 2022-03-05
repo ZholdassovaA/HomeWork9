9027db9640cc7d57262abc81d0ad8f4ead44e690
Aidana <zholdassova.aidana@mail.ru>
2022-03-06 01:11:33 +0600

    added OwnerTest

---
+++ src/test/java/properties/SystemPropertiesTest.java
@@ -0,0 +1,19 @@
+package properties;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Проверяем браузер")
+@Tag("properties")
+public class SystemPropertiesTest {
+    @Test
+    void propertiesTest() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+    }
+}
